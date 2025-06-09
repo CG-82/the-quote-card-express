e7c2cd142e0dd5fb4e767ab0637d0ef94990984a
CG-82 <christophergullett65@gmail.com>
2025-06-08 20:25:06 -0400

    added function to get random images, but removed client id for security

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,23 @@
 "use strict"
 
-const elements = {
+async function getRandomImage() {
+    const client_id = "CLIENT ID HERE";
+    const endpoint = `https://api.unsplash.com/photos/random/?client_id=${client_id}`;
+   try {
+        const response = await fetch(endpoint);
+        const returnedData = await response.json();
+        const receivedPhotoUrl = returnedData.urls.regular;
+
+        const imgDiv = document.querySelector(".background-img");
+        imgDiv.style.backgroundImage = `url(${receivedPhotoUrl})`;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+getRandomImage();
+
+/*const elements = {
     quote: document.getElementById("quote"),
     author: document.getElementById("author"),
 };
@@ -35,4 +52,5 @@ function loopThroughQuotes() {
     }, 3000);
 }
 
-setTimeout(loopThroughQuotes, 3000);
+setTimeout(loopThroughQuotes, 3000);*/
+
