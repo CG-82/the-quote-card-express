5134863dc492a726f1e3b46f1888a8ca8a9ff433
CG-82 <christophergullett65@gmail.com>
2025-06-09 17:57:30 -0400

    reworked function to hide api keys

--- public/js/index.js
+++ public/js/index.js
@@ -1,12 +1,11 @@
 "use strict"
 
 async function getRandomImage() {
-    const client_id = "CLIENT ID HERE";
-    const endpoint = `https://api.unsplash.com/photos/random/?client_id=${client_id}`;
-   try {
+    const endpoint = "http://localhost:8080/api/v1/getRandomImage";
+    try {
         const response = await fetch(endpoint);
         const returnedData = await response.json();
-        const receivedPhotoUrl = returnedData.urls.regular;
+        const receivedPhotoUrl = returnedData.data;
 
         const imgDiv = document.querySelector(".background-img");
         imgDiv.style.backgroundImage = `url(${receivedPhotoUrl})`;
